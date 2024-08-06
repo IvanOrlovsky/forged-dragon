d7835c392c49c55d3352d64672744a69de43e27e
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-08-07 00:49:29 +0300

    Пробую делаю навигацию по сайту

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import Header from "@/components/Header";
 
 export default function Home() {
-	return <main></main>;
+	let a = 1;
+	let b = a + 1;
+	a = b - a;
+	console.log(a);
+
+	return (
+		<>
+			<Header />
+			<main></main>
+		</>
+	);
 }
