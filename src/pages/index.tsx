b1efbddde79551efd54363cd4ba4a5fa3896a25b
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-08-19 01:04:48 +0300

    Добавил секцию О мастерской

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { ContentType } from "@/types/ContentType";
 import HeroSection from "@/components/HeroSection";
 import Gallery from "@/components/Gallery";
 import Contacts from "@/components/Contacts";
+import About from "@/components/About";
 
 export const getStaticProps: GetStaticProps<ContentType> = async () => {
 	const categoryDir = path.join(process.cwd(), "public", "category");
@@ -38,15 +39,13 @@ export default function Home({
 		<main className="relative min-h-screen w-full space-y-12 bg-light-text dark:bg-dark-text">
 			<NavBar />
 			<HeroSection />
-			<div
-				id="our-works"
-				className="bg-light-section dark:bg-dark-section py-12"
-			>
+			<div id="our-works" className="py-12">
 				<h2 className="pl-4 sm:pl-8 text-light-accent dark:text-dark-accent text-4xl sm:text-6xl font-bold mb-8">
 					Наши работы
 				</h2>
 				<Gallery tabs={tabs} imagesByTab={imagesByTab} />
 			</div>
+			<About />
 			<div
 				id="contacts"
 				className="bg-light-accent dark:bg-dark-accent p-5 sm:p-10 rounded-lg shadow-lg"
