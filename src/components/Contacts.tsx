b9dbe55870a40cf22163a3b54b8b139ad3c4f4fe
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-08-22 01:34:51 +0300

    Чутка подправил

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -8,35 +8,35 @@ import Location from "../../public/icons/Location.svg";
 export default function Contacts() {
 	return (
 		<>
-			<section id="contacts">
+			<section id="contacts" className="pb-6 sm:pb-10">
 				<h2 className="pl-4 sm:pl-8 text-light-text dark:text-dark-text text-4xl sm:text-6xl font-bold">
 					Свяжитесь с нами
 				</h2>
 				<div className="flex flex-col xl:flex-row p-4 xl:p-8">
-					<div className="flex flex-col xl:w-1/2 ">
+					<div className="self-center flex flex-col gap-2 xl:w-1/2 ">
 						<div>
 							<h3 className="text-2xl xl:text-3xl text-light-text dark:text-dark-text font-bold mb-5">
 								Мы в социальных сетях:
 							</h3>
 							<ul className="space-y-4">
 								<li className="flex items-center">
-									<Telegram className="w-8 h-8 mr-2" />
+									<Telegram className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
 									<a
 										href={`https://t.me/${process.env.NEXT_PUBLIC_TELEGRAM}`}
 										target="_blank"
 										rel="noopener noreferrer"
-										className="text-light-text dark:text-dark-text text-2xl"
+										className="text-light-text dark:text-dark-text text-xl sm:text-2xl"
 									>
 										Telegram
 									</a>
 								</li>
 								<li className="flex items-center">
-									<WhatsApp className="w-8 h-8 mr-2" />
+									<WhatsApp className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
 									<a
 										href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP}`}
 										target="_blank"
 										rel="noopener noreferrer"
-										className="text-light-text dark:text-dark-text text-2xl"
+										className="text-light-text dark:text-dark-text text-xl sm:text-2xl"
 									>
 										WhatsApp
 									</a>
@@ -49,10 +49,10 @@ export default function Contacts() {
 							</h3>
 							<ul className="space-y-4">
 								<li className="flex items-center">
-									<Phone className="w-8 h-8 mr-2" />
+									<Phone className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
 									<a
 										href={`tel:${process.env.NEXT_PUBLIC_PHONE_NUMBER}`}
-										className="text-black	 text-2xl font-bold"
+										className="text-black text-xl sm:text-2xl font-bold hover:underline"
 									>
 										{
 											process.env
@@ -61,17 +61,17 @@ export default function Contacts() {
 									</a>
 								</li>
 								<li className="flex items-center">
-									<Mail className="w-8 h-8 mr-2" />
+									<Mail className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
 									<a
 										href="mailto:info@yourcompany.com"
-										className="text-light-text dark:text-dark-text text-2xl"
+										className="text-light-text dark:text-dark-text text-xl sm:text-2xl hover:underline"
 									>
 										{process.env.NEXT_PUBLIC_EMAIL}
 									</a>
 								</li>
 								<li className="flex items-center">
-									<Location className="w-8 h-8 mr-2" />
-									<span className="text-light-text dark:text-dark-text text-2xl">
+									<Location className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
+									<span className="text-light-text dark:text-dark-text text-xl sm:text-2xl hover:underline">
 										Московская улица, 53А
 									</span>
 								</li>
