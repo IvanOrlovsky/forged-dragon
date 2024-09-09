88ddfa4cdeb0ba13000f12ab268c87b09fb4b5b1
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-09-09 12:49:42 +0300

    blend какой-то

--- processImages.js
+++ processImages.js
@@ -24,13 +24,13 @@ const processImage = async (imagePath, outputDir) => {
 		// Загружаем ватермарку и поворачиваем её на 45 градусов для диагонального размещения
 		const watermark = await sharp(watermarkPath)
 			.resize(Math.min(width, height)) // Изменяем размер ватермарки в зависимости от размера изображения
-			.rotate(45) // Поворот на 45 градусов для диагонали
+			.rotate(-45) // Поворот на 45 градусов для диагонали
 			.png()
 			.toBuffer();
 
 		// Создание изображения с ватермаркой по центру
 		await image
-			.composite([{ input: watermark, gravity: "center" }]) // Размещение ватермарки по центру
+			.composite([{ input: watermark, gravity: "center", blend: "over" }]) // Размещение ватермарки по центру
 			.webp({ quality: 100, lossless: true }) // Конвертация в webp с минимальной потерей качества
 			.toFile(outputImagePath);
 
